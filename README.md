356feeacbe1f87695a2fa6dacc8b8976f1b0dbe8
Nikita Cherkasov <my@ncherkasov.ru>
2023-08-03 20:38:51 +0300

    solved https://leetcode.com/problems/roman-to-integer

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 - #1 Two Sum (https://leetcode.com/problems/two-sum/)
 - #2 Add Two Numbers (https://leetcode.com/problems/add-two-numbers/)
 - #9 Palindrome Number (https://leetcode.com/problems/palindrome-number/)
+- #13 Roman to Integer (https://leetcode.com/problems/roman-to-integer)
 - #46 Permutations (https://leetcode.com/problems/permutations/)
 
 ## Plans:
---
+++ romanToInt/romanToInt.go
@@ -0,0 +1,21 @@
+package romanToInt
+
+func romanToInt(s string) int {
+	out := int(0)
+	symbols := map[uint8]int{73: 1, 86: 5, 88: 10, 76: 50, 67: 100, 68: 500, 77: 1000}
+	for i := 0; i < len(s); {
+		if i == len(s)-1 {
+			out = out + symbols[s[i]]
+			break
+		}
+		left, right := s[i], s[i+1]
+		if symbols[left] >= symbols[right] {
+			out = out + symbols[left]
+			i++
+		} else {
+			out = out + symbols[right] - symbols[left]
+			i += 2
+		}
+	}
+	return out
+}
