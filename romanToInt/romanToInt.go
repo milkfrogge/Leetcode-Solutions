1c133b5bc8ab81adeb732cdfce6d655bcff7042b
Nikita Cherkasov <trebbeat@gmail.com>
2023-08-03 20:44:43 +0300

    solved https://leetcode.com/problems/roman-to-integer

--- romanToInt/romanToInt.go
+++ romanToInt/romanToInt.go
@@ -1,6 +1,6 @@
 package romanToInt
 
-func romanToInt(s string) int {
+func RomanToInt(s string) int {
 	out := int(0)
 	symbols := map[uint8]int{73: 1, 86: 5, 88: 10, 76: 50, 67: 100, 68: 500, 77: 1000}
 	for i := 0; i < len(s); {
